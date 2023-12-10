51c89f6c1a40c778917fcc3ba9d3723dfa45b5a2
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2023-12-10 20:53:50 -0300

    before read-only volumes

--- data-volumes-01-starting-setup/docker_commands.sh
+++ data-volumes-01-starting-setup/docker_commands.sh
@@ -1,3 +1,12 @@
+docker stop data-volumes-container
+docker rm data-volumes-container
+docker rmi data-volumes-image:latest
+
 docker build -t data-volumes-image:latest .
 
-docker run -p 3000:80 -d --name data-volumes-container --rm data-volumes-image
+# -d detached
+# --name nome do container
+# --rm auto remove quando o container é parado
+# -v <nome_do_volume>:<path_dentro_do_container>
+# serve pra criar um volume nomeado ou um bind mount
+docker run -p 3000:80 -d --name data-volumes-container -v feedback_volume:/app/feedback -v "D:\Gabriel\Documentos\DockerCourse\data-volumes-01-starting-setup":/app --rm data-volumes-image
